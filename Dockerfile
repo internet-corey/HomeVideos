e035afb852b6709695fe97ecc28c8556f50bbd76
internet-corey <coreyevans90@gmail.com>
2021-07-03 14:01:26 -0600

    moved things around for docker stuff

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:14-alpine
-
-WORKDIR /HomeVideos/src
-
-COPY . /HomeVideos/
-
-RUN apk update && apk add vim
-RUN cd /HomeVideos/src && npm install
--- src/db/initial_insert.js
+++
@@ -1,5 +0,0 @@
-const films = require('./films.js');
-const scripts = require('./scripts');
-
-const query = 'INSERT INTO films (title) VALUES ?;';
-scripts.bulkInsert(query, films.films);
