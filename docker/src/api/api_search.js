b0f2d3b29594a52d0ed9ef1c57e1a026151d5da1
internet-corey <coreyevans90@gmail.com>
2021-07-08 16:55:12 -0600

    change get request from https module to axios

--- docker/src/api/api_search.js
+++ docker/src/api/api_search.js
@@ -1,28 +1,15 @@
-const apiKey = require('./api_key.js'); // .gitignore'd
-const https = require('https');
+const axios = require('axios');
+const apiKey = require('./api_key.js');
 
-function search(searchTerm, privateKey, imageSearch=false) {
+async function search(searchTerm, privateKey) {
   const key = apiKey.decrypt(apiKey.encryptedApiKey, privateKey);  // priv key gitignored
-  const url = imageSearch
-    ? `https://www.img.omdbapi.com/?apikey=${key}&t=${searchTerm}`
-    : `https://www.omdbapi.com/?apikey=${key}&t=${searchTerm}`;
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      let body = '';
-      res.on('data', (chunk) => {
-        body += chunk;
-      });
-      res.on('end', () => {
-        try {
-          resolve(body);
-        } catch (e) {
-          reject(e.message);
-        };
-      });
-    }).on('error', e => {
-      console.log('ERROR: ', e.message);
-    });
-  });
+  const url = `https://www.omdbapi.com/?apikey=${key}&t=${searchTerm}`;
+
+  try {
+    return (await axios.get(url)).data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = { search };
