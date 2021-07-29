9a5b2c22bfabf88ba755a248ff68f41c287e57a1
internet-corey <coreyevans90@gmail.com>
2021-07-28 21:08:22 -0600

    merging here on main to fix weird conflicts on other branch

--- docker/src/api/api_key.js
+++ docker/src/api/api_key.js
@@ -1,22 +1,17 @@
-const crypto = require('crypto')
-const path = require('path')
-const fs = require('fs')
-
-// found on stackoverflow 
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.encryptedApiKey = exports.decrypt = void 0;
+const crypto_1 = require("crypto");
+const fs_1 = require("fs");
 function decrypt(toDecrypt, privateKeyPath) {
-  const absolutePath = path.resolve(privateKeyPath)
-  const privateKey = fs.readFileSync(absolutePath, 'utf8')
-  const buffer = Buffer.from(toDecrypt, 'base64')
-  const decrypted = crypto.privateDecrypt(
-    {
-      key: privateKey.toString(),
-      passphrase: '',
-    },
-    buffer,
-  )
-  return decrypted.toString('utf8')
+    const privateKey = fs_1.readFileSync(privateKeyPath, 'utf8');
+    const buffer = Buffer.from(toDecrypt, 'base64');
+    const decrypted = crypto_1.privateDecrypt({
+        key: privateKey.toString(),
+        passphrase: '',
+    }, buffer);
+    return decrypted.toString('utf8');
 }
-
-const encryptedApiKey = 'gZl7J5WGU4vw6ewKoLRNHCnrSZP99Y73pfjzouQX7R5xUbbE4QlTiB9EDCuBp/Fp5wDrlQsgOX6jzIKwwd/pU95fK1TANWO+ST4iX7xbPzUEPeZuxp3z46S5sC5nFMp7wST0r5aBPZuw2mG7pH/MRCtS0mMmfD/9uHxL0VP5SZNjYmn/QSe3lEwSDST1xcDtv8v0H1frwAW0su/3hrN8x6T4s5/BepHh46OwIHpr0iH80lttJnIUWJ3B2sQlMBUh8BSQqnjcpaEQGttEhq2ZhBth1UGKITXNOJh3R07+aRWDV/UWoIyKQwY6sgehDffItAl2VtlsDpu/7OKYgEaUtaaE8vsqAwUV9HcnOKWu/nKsLsukiRn4mND13484GcAk/HiDdF9QWkQJXoiLM3HBH5AkLs6isz5IIu/1zZlEELS+4X+pjzWIE/2fuF1RCdYWy5RwGVs8rMnKTfw4TqNzsH8dwjaaiH9GVCluTBwlmCN/4Ca8lUPXREQ4FpSU5hz35n4JEwFZ5+6lBOiwpQRLG+v5E8+Cs0kRwk3G1axCJPekWTPfmEJNE6FwILmYwpl+STFfob3GKW4FtKwDjO4jhQZ4MjBPC4nHPOOEJ6HwRpV9/K6cFh0KCDbNmjud/ZgaEEsNfj2RCAYuyVMvv7lS3AWtfn/bCFWYg7zeDj1mzmo='
-
-module.exports = { decrypt, encryptedApiKey };
+exports.decrypt = decrypt;
+const encryptedApiKey = 'gZl7J5WGU4vw6ewKoLRNHCnrSZP99Y73pfjzouQX7R5xUbbE4QlTiB9EDCuBp/Fp5wDrlQsgOX6jzIKwwd/pU95fK1TANWO+ST4iX7xbPzUEPeZuxp3z46S5sC5nFMp7wST0r5aBPZuw2mG7pH/MRCtS0mMmfD/9uHxL0VP5SZNjYmn/QSe3lEwSDST1xcDtv8v0H1frwAW0su/3hrN8x6T4s5/BepHh46OwIHpr0iH80lttJnIUWJ3B2sQlMBUh8BSQqnjcpaEQGttEhq2ZhBth1UGKITXNOJh3R07+aRWDV/UWoIyKQwY6sgehDffItAl2VtlsDpu/7OKYgEaUtaaE8vsqAwUV9HcnOKWu/nKsLsukiRn4mND13484GcAk/HiDdF9QWkQJXoiLM3HBH5AkLs6isz5IIu/1zZlEELS+4X+pjzWIE/2fuF1RCdYWy5RwGVs8rMnKTfw4TqNzsH8dwjaaiH9GVCluTBwlmCN/4Ca8lUPXREQ4FpSU5hz35n4JEwFZ5+6lBOiwpQRLG+v5E8+Cs0kRwk3G1axCJPekWTPfmEJNE6FwILmYwpl+STFfob3GKW4FtKwDjO4jhQZ4MjBPC4nHPOOEJ6HwRpV9/K6cFh0KCDbNmjud/ZgaEEsNfj2RCAYuyVMvv7lS3AWtfn/bCFWYg7zeDj1mzmo=';
+exports.encryptedApiKey = encryptedApiKey;
